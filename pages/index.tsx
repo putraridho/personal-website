5169c2addb031919bab1e7e3ddd2956c01b94f60
Ridho Putra <muhammadridhoputra@gmail.com>
2023-04-09 11:28:17 +0700

    Home Hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,40 @@
 import { Container } from "@/components";
 
+import style from "./style.module.sass";
+
 export default function Home() {
 	return (
 		<main>
-			<Container>Home</Container>
+			<Container>
+				<Hero>
+					<HeroText />
+				</Hero>
+			</Container>
 		</main>
+	);
+}
+
+interface HeroProps {
+	children?: React.ReactNode;
+}
+
+function Hero(props: HeroProps): React.ReactElement {
+	return <section className={style.hero} {...props} />;
+}
+
+function HeroText(): React.ReactElement {
+	return (
+		<div className={style.heroText}>
+			<h1 className={style.heroTitle}>
+				Greetings, It&lsquo;s
+				<span className={style.big}>Ridho</span>
+			</h1>
+			<p className={style.heroSubtitle}>
+				A mortal <span className={style.accent}>Frontend Wizard</span>, casting spells to create a visually captivating
+				and intuitive websites that enchant users with seamless navigation and awe-inspiring design. With my mastery of
+				modern web technologies, I embark on epic quests to create web solutions that leave a lasting impression,
+				bringing the magic of exceptional user experiences to life in the realm of the World Wide Web.
+			</p>
+		</div>
 	);
 }
