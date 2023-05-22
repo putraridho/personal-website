c25e8dc0ea13582c6488b6fb2de21062d7b0ba96
Muhammad Ridho Putra <muhammadridhoputra@gmail.com>
2023-05-22 21:10:53 +0700

    bugfix dark toggle

--- components/DarkModeToggle/index.tsx
+++ components/DarkModeToggle/index.tsx
@@ -1,20 +1,38 @@
 import { useTheme } from "next-themes";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { RiLightbulbFill, RiLightbulbLine } from "react-icons/ri";
 
 import style from "./style.module.sass";
 
 export function DarkModeToggle(): React.ReactElement {
-	const { theme, setTheme } = useTheme();
+	const { theme, setTheme, systemTheme } = useTheme();
 
-	const ButtonIcon = useMemo(() => (theme === "dark" ? RiLightbulbFill : RiLightbulbLine), [theme]);
+	const ButtonIcon = useMemo(() => {
+		if (theme === "dark") {
+			return RiLightbulbFill;
+		}
+
+		if (theme === "system" && systemTheme === "dark") {
+			return RiLightbulbFill;
+		}
+
+		return RiLightbulbLine;
+	}, [systemTheme, theme]);
+
+	const handleToggle = useCallback(() => {
+		if (theme === "dark") {
+			return setTheme("light");
+		}
+
+		if (theme === "system" && systemTheme === "dark") {
+			return setTheme("light");
+		}
+
+		return setTheme("dark");
+	}, [setTheme, systemTheme, theme]);
 
 	return (
-		<button
-			type="button"
-			className={style.darkModeButton}
-			onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-		>
+		<button type="button" className={style.darkModeButton} onClick={handleToggle}>
 			<ButtonIcon size={20} />
 		</button>
 	);
