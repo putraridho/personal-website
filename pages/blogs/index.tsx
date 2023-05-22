45eaf674230d1ab410f0faa4caed26126aeaa169
Ridho Putra <muhammadridhoputra@gmail.com>
2023-05-22 18:33:58 +0700

    explicit getStaticProps & getStaticPaths returns

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -2,7 +2,7 @@ import { GetStaticProps } from "next";
 
 import { Container } from "@/components";
 import { query } from "@/data";
-import { IBlog, IPageBlog } from "@/types";
+import { IPageBlog } from "@/types";
 import { Hero, List } from "@/ui/blogs";
 
 export default function Blog({ items }: IPageBlog) {
@@ -17,26 +17,32 @@ export default function Blog({ items }: IPageBlog) {
 }
 
 export const getStaticProps: GetStaticProps<IPageBlog> = async () => {
-	let has_more = false;
-	let next_cursor = null;
-	let items: IBlog[] = [];
-
 	try {
 		const res = await query(process.env.NEXT_PUBLIC_NOTION_DATABASE_ID);
 		if (res.success) {
-			has_more = res.has_more;
-			next_cursor = res.next_cursor;
-			items = res.results;
+			return {
+				props: {
+					has_more: res.has_more,
+					next_cursor: res.next_cursor,
+					items: res.results,
+				},
+			};
 		}
 	} catch (err) {
-		// Do Nothing
+		return {
+			props: {
+				has_more: false,
+				next_cursor: null,
+				items: [],
+			},
+		};
 	}
 
 	return {
 		props: {
-			has_more,
-			next_cursor,
-			items,
+			has_more: false,
+			next_cursor: null,
+			items: [],
 		},
 	};
 };
