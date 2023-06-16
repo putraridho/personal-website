7e6a0215ae000fc919a8979b147c29a8e6c1a3b1
Ridho Putra <muhammadridhoputra@gmail.com>
2023-06-16 19:27:37 +0700

    banner content flashlight effect

--- ui/new-home/index.tsx
+++ ui/new-home/index.tsx
@@ -1,15 +1,34 @@
-import { ArrowRight, MagicWand } from "@/icons";
-import style from "./style.module.sass";
+import { EnvelopeClosedIcon, GitHubLogoIcon, InstagramLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import classNames from "classnames";
 import Image from "next/image";
 import Link from "next/link";
-import classNames from "classnames";
-import { EnvelopeClosedIcon, GitHubLogoIcon, InstagramLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { MouseEventHandler, useCallback, useRef } from "react";
+
+import { ArrowRight, MagicWand } from "@/icons";
+
+import style from "./style.module.sass";
 
 export function NewHome(): React.ReactElement {
+	const bannerContentRef = useRef<HTMLDivElement>(null);
+
+	const handleMouseMove = useCallback<MouseEventHandler<HTMLDivElement>>(
+		(e) => {
+			bannerContentRef?.current?.style.setProperty(
+				"--mouse-x",
+				(e.pageX || 0 - bannerContentRef?.current?.clientLeft || 0) + "px",
+			);
+			bannerContentRef?.current?.style.setProperty(
+				"--mouse-y",
+				(e.pageY || 0 - bannerContentRef?.current?.clientTop || 0) + "px",
+			);
+		},
+		[bannerContentRef],
+	);
+
 	return (
 		<div className={style.wrapper}>
 			<div className={style.banner}>
-				<div className={style.bannerContent}>
+				<div ref={bannerContentRef} className={style.bannerContent} onMouseMove={handleMouseMove}>
 					<div className={style.bannerContainer}>
 						<MagicWand className={style.magicWand} />
 						<p className={style.bannerText}>
